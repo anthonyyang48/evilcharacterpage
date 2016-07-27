bb8f0b2daeef416ceebe780ae826c361936c22e6
anthonyyang48 <wangrusherry12@gmail.com>
2016-07-27 16:47:49 +0000

    Message

---
+++ script.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    console.log('test')
+    var count = 0;
+    var count1 = 0;
+    
+    $(".btn").on('click',function(){
+       count++
+    $("#number").text('count')
+    })
+    
+});
